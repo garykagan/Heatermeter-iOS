4178500ca4a50b954beb33951e02436271ba81f2
Gary Kagan <gary.kagan@siriusxm.com>
2022-11-26 08:34:28 -0500

    Initial Commit

---
+++ Heatermeter/HeatermeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeatermeterApp.swift
+//  Heatermeter
+//
+//  Created by Gary Kagan on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct HeatermeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
